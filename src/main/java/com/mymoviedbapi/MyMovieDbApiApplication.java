6ffcc58134633742b435fa8080fe5151a49b8743
Jakub <01142557@pw.edu.pl>
2020-10-15 21:55:35 +0200

    XYZ-005 Dodanie strony powitalnej

--- src/main/java/com/mymoviedbapi/MyMovieDbApiApplication.java
+++ src/main/java/com/mymoviedbapi/MyMovieDbApiApplication.java
@@ -2,12 +2,22 @@ package com.mymoviedbapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MyMovieDbApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyMovieDbApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyMovieDbApiApplication.class, args);
+    }
+
+    @RestController
+    class HelloController {
+        @GetMapping("/")
+        String hello() {
+            return "Hello World";
+        }
+    }
 
 }
