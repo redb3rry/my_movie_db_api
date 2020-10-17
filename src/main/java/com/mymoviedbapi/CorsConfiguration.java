c8fb75f9fb6a81370039a571caf8b3221b319cdf
redb3rry <michalmalinowski99@yahoo.co.uk>
2020-10-18 01:02:39 +0200

    Połączenie z frontendem próba 8

--- src/main/java/com/mymoviedbapi/CorsConfiguration.java
+++ src/main/java/com/mymoviedbapi/CorsConfiguration.java
@@ -1,18 +1,17 @@
 package com.mymoviedbapi;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfiguration {
-    @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/api/v1/movies/").allowedOrigins("http://localhost:3000");
-            }
-        };
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
     }
 }
