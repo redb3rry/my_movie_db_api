3003a146774b75fbfeb9e49f831dc1a1484dddb5
redb3rry <michalmalinowski99@yahoo.co.uk>
2020-10-18 00:52:20 +0200

    zmieniono allowedOrigin na http://localhost:3000

--- src/main/java/com/mymoviedbapi/CorsConfiguration.java
+++ src/main/java/com/mymoviedbapi/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/api/v1/movies/").allowedOrigins("http://localhost:3000/");
+                registry.addMapping("/api/v1/movies/").allowedOrigins("http://localhost:3000");
             }
         };
     }
