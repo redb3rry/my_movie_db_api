eea75dc8472b1e053f770455d0840468e709b685
czajkaj <01142557@pw.edu.pl>
2020-10-22 13:07:00 +0200

    Dodatnie mapowania do metody OPTIONS

--- src/main/java/com/mymoviedbapi/MovieController.java
+++ src/main/java/com/mymoviedbapi/MovieController.java
@@ -60,11 +60,11 @@ public class MovieController {
         return ResponseEntity.ok(updatedMovie);
     }
 
-    @RequestMapping(value = "/option/", method = RequestMethod.OPTIONS)
+    @RequestMapping(value = "/options/", method = RequestMethod.OPTIONS)
     public ResponseEntity options(){
         return ResponseEntity
                 .ok()
-                .allow(HttpMethod.GET, HttpMethod.POST, HttpMethod.OPTIONS)
+                .allow(HttpMethod.GET, HttpMethod.POST, HttpMethod.OPTIONS, HttpMethod.DELETE, HttpMethod.PUT)
                 .build();
     }
 
